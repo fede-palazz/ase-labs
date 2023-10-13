ac220759ee34a1be484161960ed76dd92d93a6a7
fede-palazz <fede.palazzi1999@gmail.com>
2023-10-13 13:07:50 +0200

    Updated array values

--- Lab_1 11-10-2023/program_0.s
+++ Lab_1 11-10-2023/program_0.s
@@ -6,7 +6,7 @@
     .data
 v1:  	.byte   -2, 6, -3, 1, 9, 1, -3, 6, -2
 v2:  	.byte   4, 7, -10, 3, 11, 9, 7, 6, 4, 7
-v3:  	.byte   8, 2, 5, -1, -4, -1, 5, 2, 9
+v3:  	.byte   9, 2, 5, -1, -4, -1, 5, 2, 9
 ; Variables are inizialized to zero by default
 fl1: 	.space 1
 fl2: 	.space 1
@@ -35,7 +35,7 @@ LOOP1:
         lb R3,v1(R1)
         lb R4,v1(R2)
         ; Check if paired values are the same
-        bne R3,R4,NOT1  ; array not palindrome -> jump to NOT1
+        bne R3,R4,NOT1
         ; Update pointers
         daddi R1,R1,1       ; R1++
         daddi R2,R2,-1      ; R2--
@@ -48,14 +48,14 @@ LOOP1:
 NOT1:
         ; Reset array pointers
         daddi R1,R0,0    ; R1 = 0  POINTER REG LEFT
-        daddi R2,R0,8    ; R2 = 8 POINTER REG RIGHT
+        daddi R2,R0,8    ; R2 = 8  POINTER REG RIGHT
 
 LOOP2:   
         ; Load value from v2
         lb R3,v2(R1)
         lb R4,v2(R2)
         ; Check if paired values are the same
-        bne R3,R4,NOT2  ; array not palindrome -> jump to NOT2
+        bne R3,R4,NOT2
         ; Update pointers
         daddi R1,R1,1       ; R1++
         daddi R2,R2,-1      ; R2--
@@ -75,7 +75,7 @@ LOOP3:
         lb R3,v3(R1)
         lb R4,v3(R2)
         ; Check if paired values are the same
-        bne R3,R4,NOT3  ; array not palindrome -> jump to NOT3
+        bne R3,R4,NOT3
         ; Update pointers
         daddi R1,R1,1       ; R1++
         daddi R2,R2,-1      ; R2--
