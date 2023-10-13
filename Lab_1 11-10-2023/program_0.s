8761fcd71ff1e1262d89217f95ae421b7a1ba89d
fede-palazz <fede.palazzi1999@gmail.com>
2023-10-13 13:04:23 +0200

    Implemented palindrome check using only one loop

--- Lab_1 11-10-2023/program_0.s
+++ Lab_1 11-10-2023/program_0.s
@@ -6,7 +6,7 @@
     .data
 v1:  	.byte   -2, 6, -3, 1, 9, 1, -3, 6, -2
 v2:  	.byte   4, 7, -10, 3, 11, 9, 7, 6, 4, 7
-v3:  	.byte   8, 2, 5, -1, -4, -1, 5, 2, 9
+v3:  	.byte   9, 2, 5, -1, -4, -1, 5, 2, 9
 ; Variables are inizialized to zero by default
 fl1: 	.space 1
 fl2: 	.space 1
@@ -22,100 +22,96 @@ MAIN:
         daddi R3,R0,0           ; R3 = 0  TEMP REG 1
         daddi R4,R0,0           ; R4 = 0  TEMP REG 2
         ; Inizialize flag registers
-        daddi R5,R0,0           ; R5 = 0  FLAG_1 REG
-        daddi R6,R0,0           ; R6 = 0  FLAG_2 REG
-        daddi R7,R0,0           ; R7 = 0  FLAG_3 REG
+        daddi R5,R0,1           ; R5 = 1  FLAG_1 REG
+        daddi R6,R0,1           ; R6 = 1  FLAG_2 REG
+        daddi R7,R0,1           ; R7 = 1  FLAG_3 REG
 
 ;-------------------------------------------
 ; POINT 1: Check if arrays are palindrome
 ;-------------------------------------------
 
-LOOP1:   
+CHECK_V1:
+        ; Check if it was marked as NOT palindrome
+        beqz R5,CHECK_V2
         ; Load value from v1
         lb R3,v1(R1)
         lb R4,v1(R2)
         ; Check if paired values are the same
-        bne R3,R4,NOT1  ; array not palindrome -> jump to NOT1
-        ; Update pointers
-        daddi R1,R1,1       ; R1++
-        daddi R2,R2,-1      ; R2--
-        ; Check whether the scan is finished
-        bne R1,R2,LOOP1     ; continue the loop
-        ; Palindrome array -> update flag 1
-        daddi R5,R0,1       ; R5 = 1
-        sb R5,fl1(R0)       ; fl1 = 1
+        bne R3,R4,NOT1
+        j CHECK_V2
 
 NOT1:
-        ; Reset array pointers
-        daddi R1,R0,0    ; R1 = 0  POINTER REG LEFT
-        daddi R2,R0,8    ; R2 = 8 POINTER REG RIGHT
+        ; v1 not palindrome -> update flag register
+        daddi R5,R0,0           ; R5 = 0
 
-LOOP2:   
+
+CHECK_V2:
+        ; Check if it was marked as NOT palindrome
+        beqz R6,CHECK_V3
         ; Load value from v2
         lb R3,v2(R1)
         lb R4,v2(R2)
         ; Check if paired values are the same
-        bne R3,R4,NOT2  ; array not palindrome -> jump to NOT2
-        ; Update pointers
-        daddi R1,R1,1       ; R1++
-        daddi R2,R2,-1      ; R2--
-        ; Check whether the scan is finished
-        bne R1,R2,LOOP2     ; continue the loop
-        ; Palindrome array -> update flag 2
-        daddi R6,R0,1       ; R6 = 1
-        sb R6,fl2(R0)       ; fl2 = 1
+        bne R3,R4,NOT2
+        j CHECK_V3
 
 NOT2:
-        ; Reset array pointers
-        daddi R1,R0,0    ; R1 = 0  POINTER REG LEFT
-        daddi R2,R0,8    ; R2 = 8 POINTER REG RIGHT
+        ; v2 not palindrome -> update flag register
+        daddi R6,R0,0           ; R6 = 0
 
-LOOP3:   
+CHECK_V3:
+        ; Check if it was marked as NOT palindrome
+        beqz R7,CHECK_LOOP
         ; Load value from v3
         lb R3,v3(R1)
         lb R4,v3(R2)
         ; Check if paired values are the same
-        bne R3,R4,NOT3  ; array not palindrome -> jump to NOT3
-        ; Update pointers
-        daddi R1,R1,1       ; R1++
-        daddi R2,R2,-1      ; R2--
-        ; Check whether the scan is finished
-        bne R1,R2,LOOP3     ; continue the loop
-        ; Palindrome array -> update flag 3
-        daddi R7,R0,1       ; R7 = 1
-        sb R7,fl3(R0)       ; fl3 = 1
+        bne R3,R4,NOT3
+        j CHECK_LOOP
 
 NOT3:
-        ; Reset array pointers
-        daddi R1,R0,0    ; R1 = 0  POINTER REG LEFT
-        daddi R2,R0,8   ; R2 = 8 POINTER REG RIGHT
-        ; Reset temp registers
-        daddi R3,R0,0    ; R3 = 0
-        daddi R4,R0,0    ; R4 = 0
+        ; v3 not palindrome -> update flag register
+        daddi R7,R0,0           ; R7 = 0
+        
+CHECK_LOOP:
+        ; Update pointers
+        daddi R1,R1,1           ; R1++
+        daddi R2,R2,-1          ; R2--
+        ; Check whether the scan is finished
+        bne R1,R2,CHECK_V1      ; continue the loop
+        ; Reset pointers registers
+        daddi R1,R0,0           ; R1 = 0
+        daddi R2,R0,8           ; R2 = 8
+        ; Reset temp register
+        daddi R4,R0,0           ; R4 = 0
+        ; Save flag registers values
+        sb R5,fl1(R0)
+        sb R6,fl2(R0)
+        sb R7,fl3(R0)
 
 ;---------------------------------------------------
 ; POINT 2: Sum of the elements in palindrome arrays
 ;---------------------------------------------------
 
-CHECK_V1:
+SUM_V1:
         ; Check whether v1 is palindrome
         ; R5 = 0 => v1 NOT palindrome => skip to v2
-        beqz R5,CHECK_V2
+        beqz R5,SUM_V2
         ; v1 is palindrome => sum v1[R1] in R4
         lb R3,v1(R1)
         ; R4 -> Accumulator register
         dadd R4,R4,R3
 
-CHECK_V2:
+SUM_V2:
         ; Check whether v2 is palindrome
         ; R6 = 0 => v2 NOT palindrome => skip to v3
-        beqz R6,CHECK_V3
+        beqz R6,SUM_V3
         ; v2 is palindrome => sum v2[R1] in R4
         lb R3,v2(R1)
         ; R4 -> Accumulator register
         dadd R4,R4,R3
 
-CHECK_V3:
+SUM_V3:
         ; Check whether v3 is palindrome
         ; R7 = 0 => v3 NOT palindrome => skip
         beqz R7,UPDATE_REGS
@@ -137,7 +133,7 @@ UPDATE_REGS:
         ; if R2 < R1 => exit the loop
         slt R3,R2,R1
         ; R3 = 0 => continue the loop
-        beqz R3,CHECK_V1
+        beqz R3,SUM_V1
 
 END:
         HALT
